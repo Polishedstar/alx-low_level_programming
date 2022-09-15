f9d07775d813bf7f9da4272556818b5c97adfa98
Solomon Apochi <apochisolomon404a@gmail.com>
2022-09-14 19:10:47 -0700

    My lowercase check

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,16 @@
+#include <main.h>
+/**
+ * print_alphabet - print lowercase alphabets
+ *
+ * Return: Always success
+ */
+void print_alphabet(void)
+{
+	int n;
 
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar("\n");
+}
