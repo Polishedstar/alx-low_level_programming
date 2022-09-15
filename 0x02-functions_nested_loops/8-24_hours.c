4ea7ad4570ab79c94c12906cfa1ec5a4cf51248e
Polishedstar <111003992+Polishedstar@users.noreply.github.com>
2022-09-14 21:06:42 -0700

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,22 +1,46 @@
 #include "main.h"
 /**
- * jack_bauer - function that prints every minute of the day of Jack Bauer
- * h = hour, m = minutes
- * Return: Always success
+ * jack_bauer - prints out every minute of the day for jack bauer
+ *
+ * Description: Prints out every minute of the day for jack bauer.
+ * Return: Void
  */
+
 void jack_bauer(void)
 {
-	int h, m;
-	for (h = 0; h < 24; h++)
-	{
-		for (m = 0; m < 60; m++)
-		{
-			_putchar((h / 10) + "0");
-			_putchar((h % 10) + "0");
-			_putchar(":");
-			_putchar((m / 10) + "0");
-			_putchar((m % 10) + "0");
-			_putchar(*\n*);
-		}
-	}
+int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
+hours_max = 58;
+hours_tens = '0';
+while (hours_tens < '3')
+{
+if (hours_tens == '2')
+{
+hours_max = '4';
+}
+hours_ones = '0';
+while (hours_ones < hours_max)
+{
+minutes_tens = '0';
+while (minutes_tens < '6')
+{
+minutes_ones = '0';
+while (minutes_ones < 58)
+{
+_putchar(hours_tens);
+_putchar(hours_ones);
+_putchar(':');
+_putchar(minutes_tens);
+_putchar(minutes_ones);
+_putchar('\n');
+minutes_ones++;
+}
+minutes_ones = '0';
+minutes_tens++;
+}
+minutes_tens = '0';
+hours_ones++;
+}
+hours_ones = '0';
+hours_tens++;
+}
 }
