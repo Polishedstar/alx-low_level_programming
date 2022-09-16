08bcc10a1aed921a6099cfc289159200e283153c
Polishedstar <111003992+Polishedstar@users.noreply.github.com>
2022-09-15 19:55:00 -0700

    Update 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * more_numbers - prints numbers 10 times
+ *
+ * Description: prints out numbers from 0 to 14 to the
+ * terminal ten times.
+ *  Return: Void
+ */
+void more_numbers(void)
+{
+int times;
+int n;
+for (times = 0; times < 10; times++)
+{
+n = 0;
+while (n < 15)
+{
+int curr;
+curr = n;
+if (curr / 10)
+{
+putchar('0' + (curr / 10));
+curr = curr % 10;
+}
+putchar('0' + curr);
+n++;
+}
+putchar('\n');
+}
+}
