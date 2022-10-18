c183a920f74a65e2a2c50eef1c1e52018e69e56b
Polishedstar <111003992+Polishedstar@users.noreply.github.com>
2022-10-17 19:42:33 -0700

    Hot fix

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - some program.compilitation take into
+ * consideration the main.c file before compiling.
+ * Return: void
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
