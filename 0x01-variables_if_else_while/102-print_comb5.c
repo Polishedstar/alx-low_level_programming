9f67fb251d74d44acf7084a52db1e2bef9dcab59
Polishedstar <111003992+Polishedstar@users.noreply.github.com>
2023-06-21 20:24:35 -0700

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,48 +1,42 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 /**
  * main - main block
- * Description: Write a program that prints all possible
- * different combinations of two digits
+ * Description: Print all possible combinations of two two-digit numbers.
+ * Numbers should range from 0 to 99.
+ * The two numbers should be separated by a space.
+ * All numbers should be printed with two digits. 1 should be printed as 01.
+ * Combination of numbers must be separated by a comma followed by a space.
+ * Combinations of numbers should be printed in ascending order.
+ * `00 01` and `01 00` are considered as the same combination.
+ * You can only use `putchar` to print to console.
+ * You can only use `putchar` up to 8 times.
+ * You are not allowed to use any variable of type `char`.
  * Return: 0
  */
+
 int main(void)
 {
-	int c;
-	int d;
-	int e;
-	int f = 0;
-	while (f < 10)
+	int x, y;
+
+	for (x = 0; x < 100; x++)
 	{
-		e = 0;
-		while (e < 10)
+		for (y = 0; y < 100; y++)
 		{
-			d = 0;
-			while (d < 10)
+			if (x < y)
 			{
-				c = 0;
-				while (c < 10)
+				putchar((x / 10) + 48);
+				putchar((x % 10) + 48);
+				putchar(' ');
+				putchar((y / 10) + 48);
+				putchar((y % 10) + 48);
+				if (x != 98 || y != 99)
 				{
-					if (!(f == c && e == d))
-					{
-						putchar('0' + f);
-						putchar('0' + e);
-						putchar(' ');
-						putchar('0' + d);
-						putchar('0' + c);
-						if (!(f + e == 18 && c + d == 17 && d == 9))
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
-					c++;
+					putchar(',');
+					putchar(' ');
 				}
-				d++;
 			}
-			e++;
 		}
-		f++;
 	}
 	putchar('\n');
 	return (0);
